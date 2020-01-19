abef19f29e6fd431986f786168ec0c48a69b4616
martinverdejo <martin@sohoapp.com>
2020-01-20 00:46:47 +0800

    Remove hard-coded values

--- app/workers/index_reader_worker.rb
+++ app/workers/index_reader_worker.rb
@@ -2,7 +2,7 @@ class IndexReaderWorker < BaseWorker
 
   def perform
     IndexReaderService.packages.first(50).each do |detail|
-      version = Version.joins(:package).where("version_number = '1.0.0' AND packages.name = 'A3'").last
+      version = Version.joins(:package).where("version_number = ? AND packages.name = ?", detail['Version'].to_s, detail['Package']).last
       CreateVersionWorker.perform_async(detail['Package'], detail['Version'].to_s) unless version
     end
   end
